41b93099e5152d52508d0f108b24d0a2bba07f99
Gustavo Cruz Pinheiro <54116994+Gustavo-Cruz-Pinheiro@users.noreply.github.com>
2024-10-30 11:53:36 -0300

    Update database.js

--- src/config/database.js
+++ src/config/database.js
@@ -2,7 +2,7 @@ module.exports = {
   dialect: "postgres",
   host: "localhost",
   username: "postgres",
-  password: "rootcruz",
+  password: "******",
   database: "top",
   define: {
     timestamps: true,
